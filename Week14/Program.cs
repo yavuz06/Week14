63527032dcae2a72d552170cbdff2a92c9a564c3
yavuz06 <yavuzselmkaya@gmail.com>
2023-05-17 16:06:38 +0300

    week14

---
+++ Week14/Program.cs
@@ -0,0 +1,37 @@
+﻿Dog goodBoy = new Dog("Good Boy");
+Console.WriteLine($"Name:{goodBoy.Name}; level of happinnes:{goodBoy.LevelOfHappines}");
+while (goodBoy.LevelOfHappines < 5)
+{
+    goodBoy.Bark();
+}
+goodBoy.Wag();
+
+class Dog
+{
+    public string Name;
+    public int LevelOfHappines;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappines = 0;
+        Console.WriteLine($"{Name} is your friend now");
+    }
+    public void Bark()
+    {
+        LevelOfHappines++;
+        Console.WriteLine("woof-woof");
+
+
+    }
+    public void Wag()
+    {
+        Console.WriteLine($"{Name} stars wagging its tail.");
+        Console.WriteLine("Whoosh-whoosh");
+
+
+    }
+
+
+
+}
